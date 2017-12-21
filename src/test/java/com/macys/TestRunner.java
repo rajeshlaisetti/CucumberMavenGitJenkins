8d45295feaef3cc798c63acb2132674c2af7f47a
https://github.com/rajeshlaisetti <rajeshvarmalaisetti@gmail.com>
2017-12-21 21:55:26 +0530

    Cucumber updated with runner file

---
+++ src/test/java/com/macys/TestRunner.java
@@ -0,0 +1,21 @@
+package com.macys;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features="classpath:ApplicationLogin.feature",
+        glue ={"classpath:com.macys"},
+        tags={"@Login"},
+        plugin = {"pretty", "html:target/cucumber-html-report"},
+        dryRun = false,
+        monochrome = true
+)
+
+
+public  class TestRunner {
+
+}
